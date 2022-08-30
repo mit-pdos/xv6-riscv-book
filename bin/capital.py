90ccefed07bc6897140f9d08696082abf1502089
Frans Kaashoek <kaashoek@mit.edu>
2022-08-30 12:50:07 -0400

    Don't capitalize code names

--- bin/capital.py
+++ bin/capital.py
@@ -3,13 +3,23 @@
 import re
 import sys
 
-regex = re.compile('\.\s+([a-z]+[a-zA-Z0-9]+)')
+# look for uncapitalized xv6
+regexsmall = re.compile(r'\.\s+(xv6)')
+
+with open(sys.argv[1], 'r') as f:
+    d = f.read()
+    for w in re.findall(regexsmall, d):
+        print("Error smallcaps %s: %s" % (sys.argv[1], w))
+
+# look for capitalized code names (e.g., \lstinline{Exec}), but
+# names that are all caps
+regexbig = re.compile(r'\\(lstinline|indexcode){([A-Z][a-z]+[a-zA-Z_]+)')
 
 with open(sys.argv[1], 'r') as f:
     line = f.readline()
     cnt = 1
     while line:
-        for w in re.findall(regex, line):
-            print("%s:%d %s" % (sys.argv[1], cnt, w))
+        for w in re.findall(regexbig, line):
+            print("%s:%d: error: %s" % (sys.argv[1], cnt, w[1]))
         line = f.readline()
         cnt += 1
